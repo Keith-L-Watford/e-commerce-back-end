738e1bdce5577695fb3cd1858444937faa001fe2
Keith Watford <keith.l.watford@gmail.com>
2021-07-09 19:38:39 -0400

    was missing an 'async'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -73,7 +73,7 @@ router.put('/:id', (req, res) => {
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Product.destroy({
