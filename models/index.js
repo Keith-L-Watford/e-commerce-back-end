3dd263dd6e97b3e6b769e1257a21ddad65efeeac
Keith Watford <keith.l.watford@gmail.com>
2021-07-09 18:44:15 -0400

    did the category to production relationship

--- models/index.js
+++ models/index.js
@@ -5,11 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
