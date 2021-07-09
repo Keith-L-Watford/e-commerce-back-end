5562d98760f35d2abf605a3ce420d6e53ca10f98
Keith Watford <keith.l.watford@gmail.com>
2021-07-09 18:24:59 -0400

    almost done, just need to add some validations

--- models/Product.js
+++ models/Product.js
@@ -9,11 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: {},
-    product_name: {},
-    price: {},
-    stock: {},
-    category_id: {},
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // ADD VALIDIATION OF DECIMAL
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      // VALIDATE THE VALUE IS NUMERIC
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
