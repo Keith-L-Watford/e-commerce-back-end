44fac773be8431b0610298f43d0d5597cd0120a4
Keith Watford <keith.l.watford@gmail.com>
2021-07-09 21:11:03 -0400

    still need to look at create, but i think everything else is right

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,12 +44,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// UNSURE ABOUT CREATE - BUT IM PRETTY CERTAIN EVERYTHING ELSE IS WRITTEN CORRECTLY
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Product.create({
-      category_name: req.body.category_name,
-    });
+    const categoryData = await Product.create(
+   req.body
+    );
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
