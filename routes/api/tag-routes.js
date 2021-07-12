c5fd8a996ee2661a63de2bbadbb3e07880f80de6
Keith Watford <keith.l.watford@gmail.com>
2021-07-12 19:42:54 -0400

    finished tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,22 +52,27 @@ router.post('/', async (req, res) => {
   }
 });
 
- // update a tag's name by its `id` value ??????
-router.put('/:id', (req, res) => {
-  Tag.update(
-    {
-      id: req.body.id,
-      tag_name: req.body.tag_name,
-    },
-    {
+ // update a tag's name by its `id` value ✅
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(
+      {
+        id: req.body.id,
+        tag_name: req.body.tag_name,
+      },
+      {
       where: {
         id: req.params.id,
       },
-    },
-  )
-  .then((updatedTag) => {
-    res.json(updatedTag);
-  });
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tags with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
   // delete on tag by its `id` value  ✅
