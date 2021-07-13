592cf44ec3fc11535e1083c3d76d9c3c3c9021b7
Keith Watford <keith.l.watford@gmail.com>
2021-07-12 20:23:32 -0400

    i think its done

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productData = await Product.findAll({ 
       include: [Category, { model: Tag, through: ProductTag}]
     });
     res.status(200).json(productData);
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{model: Product}],
+      include: [Category, { model: Tag, through: ProductTag}]
     });
 
     if (!productData) {
